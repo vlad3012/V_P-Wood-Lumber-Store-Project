cde8c46657a73a177a9b36f59f7ddfcdeb532163
Vladislav Petrikanets <vpetrykanets@gmail.com>
2022-07-30 19:50:30 +0300

    corrections UserRepository

--- src/main/java/com/example/lumberstore/repository/UserRepository.java
+++ src/main/java/com/example/lumberstore/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.example.lumberstore.repository;
 import com.example.lumberstore.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PersonRepository extends CrudRepository<User,Integer> {
-    User findById(int id);
+public interface UserRepository extends CrudRepository<User,Integer> {
+    User findById(Long id);
     User findByName(String name);
     User findByFirstname(String firstName);
     User findByLastname(String lastName);
     User findByEmail(String email);
+    void deleteById(int id);
+    void deleteByUsername(String userName);
 }
